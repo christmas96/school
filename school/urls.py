343a4ffd8a12b0da471205d6285a0584a6d73785
Nik <nikibar4@gmail.com>
2018-12-12 01:40:21 +0200

    Revert unwanted changes

--- school/urls.py
+++ school/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.UserProfile.loginUser, name='login'),
     path('signup', views.UserProfile.registerUser, name='signup'),
-    # path('user/schedule', views.UserProfile.get_user_schedule, name='get_user_schedule'),
+    path('user/schedule', views.UserProfile.get_user_schedule, name='get_user_schedule'),
     path('teacher/subjects', teacher.TeacherProfile.get_subjects, name='get_teacher_all_subjects'),
     path('teacher/groups', teacher.TeacherProfile.get_groups, name='get_teacher_all_groups'),
-    # path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls)
 ]
