7c517d4da719de7421a0ee3bf20f93679b130fa3
Nik <nikibar4@gmail.com>
2018-12-11 22:24:39 +0200

    Add db to db.dev

--- school/urls.py
+++ school/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.UserProfile.loginUser, name='login'),
-    path('signup', views.UserProfile.registerUser, name='signup'),
-    path('user/schedule', views.UserProfile.get_user_schedule, name='get_user_schedule')
+    path('signup', views.UserProfile.registerUser, name='signup')
 ]
--- svc_school/settings.py
+++ svc_school/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'school.urls'
+ROOT_URLCONF = 'svc_school.urls'
 
 TEMPLATES = [
     {
